d5180e67e2da02e0e3b028256371511b29d3d951
Cauã <caua.richlin@ekyte.com>
2025-09-02 11:52:28 -0300

    Aula 06

---
+++ Aula06/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula05
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("============Verificador de Numeros Pares ou Ímpares========");
+            Console.WriteLine("Digite um numero; ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = number % 2;
+            if (result == 0)
+            {
+                Console.WriteLine("O numero digitado é par!");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar!");
+            }
+        }
+    }
+}
