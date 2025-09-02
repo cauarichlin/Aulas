cd53f4d1f32068d6b2b2ebddf5fef0aebc1f2cc7
Cauã <caua.richlin@ekyte.com>
2025-09-02 11:01:16 -0300

    Aula03

---
+++ Aula03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Aula03;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        //var 
+        //dynamic
+        //obj
+
+        dynamic varible = 20;
+
+        variable = "caua";
+
+        Console.WriteLine("O valor da variavel é:" + variable);
+
+    }
+    }
