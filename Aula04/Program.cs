4bdceaec5d6d40f65d5631e7788f13ccf9367f2f
Cauã <caua.richlin@ekyte.com>
2025-09-02 11:14:05 -0300

    Aula04

---
+++ Aula04/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula04;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-----CADASTRO DE USUARIO-----");
+
+        //Solicitar o nome do Usuario
+        Console.WriteLine("Digite Seu Nome:");
+        string name = Console.ReadLine();
+
+
+        //Solicitar a Idade do Usuario 
+        Console.WriteLine("Digite Sua Idade: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("----------");
+
+        Console.WriteLine("Nome do Usuario: " + name);
+        Console.WriteLine("Idade do Usuario: " + age);
+    }
+}
