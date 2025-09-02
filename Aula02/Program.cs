ce3fc69c6293ed5a92586052a5d5989ac9590c3d
Cauã <caua.richlin@ekyte.com>
2025-09-02 10:49:31 -0300

    Aula02

---
+++ Aula02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Aula02;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        string myName = "Cauã";
+        int number = 20;
+        float numberfloat = 10.50;
+        chat character = 'a';
+        bool boolean = true;
+
+        Console.WriteLine(numberFloat);
+    }
+}
