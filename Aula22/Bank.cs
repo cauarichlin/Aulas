7123c53472fea9fb1f518e0a3843adb551b7a4e8
Cauã <caua.richlin@ekyte.com>
2025-09-04 10:33:53 -0300

    Aula22

---
+++ Aula22/Bank.cs
@@ -0,0 +1,13 @@
+namespace Aula23
+{
+    public class Bank
+    {
+        public int Id;
+        public string Name;
+
+        public void RequestNewCard()
+        {
+            Console.WriteLine("Enviando Email...(Solicite um novo cartão, ligando para o número: 8888-8888)");
+        }
+    }
+}
---
+++ Aula22/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aula22;
+
+public class Program
+{
+    public static void Main()
+    {
+        var bankPraiano = new Bank();
+        bankPraiano.Name = "Banco Praiano";
+        bankPraiano.Id = 02545;
+
+        bankPraiano.RequestNewCard();
+    }
+}
